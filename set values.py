4d0c881a7210ceb83c630f93bf606464933a7e0b
khushbooshali <113338487+khushbooshali@users.noreply.github.com>
2023-12-03 22:24:34 +0530

    Add files via upload

---
+++ set values.py
@@ -0,0 +1,9 @@
+def display_dict(dictionary):
+    for key, value in dictionary.items():
+        print(f"Key: {key}, Value: {value}")
+
+# Example dictionary
+my_dict = {'ramu':25,'ajay':5,'satya':50}
+
+# Display keys and values
+display_dict(my_dict)
