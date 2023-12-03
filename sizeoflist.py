1ad3554de98ef7188b5fed77580842eccc71ba15
khushbooshali <113338487+khushbooshali@users.noreply.github.com>
2023-12-04 03:10:37 +0530

    sizeoflist.py

--- sizeoflist.py
+++ sizeoflist.py
@@ -1,3 +1,7 @@
+Question No 3:- write a fuction that takes a list ad returns a new list with 
+                unique elements of the first list and repeated elements will 
+                be removed.
+
 def remove_duplicates(input_list):
     unique_list = []
     for item in input_list:
