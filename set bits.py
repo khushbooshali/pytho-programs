36d54a044ecb1f8375682b3f82fba8b0ecc3e774
khushbooshali <113338487+khushbooshali@users.noreply.github.com>
2023-12-04 03:17:04 +0530

    set bits.py

--- set bits.py
+++ set bits.py
@@ -1,3 +1,6 @@
+Question No 1 :- wap to count set Bits in an integer using functions.
+                (the program finds the number of ones in the binary representstion of a number.)
+
 def count_set_bits(number):
     binary_representation = bin(number)
     set_bits_count = binary_representation.count('1')
