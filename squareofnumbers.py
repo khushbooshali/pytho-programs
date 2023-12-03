9c6a62cd66c3f79943c6f5a583d3b65b9e8fa459
khushbooshali <113338487+khushbooshali@users.noreply.github.com>
2023-12-04 03:08:26 +0530

    squareofnumbers.py

--- squareofnumbers.py
+++ squareofnumbers.py
@@ -1,6 +1,11 @@
+ Question No 4 :-write a function to create and print a list where 
+                  the values are square of numbers betwween 1 and n 
+                 (both included).
+                  
 def create_square_list(n):
     square_list = [i**2 for i in range(1, n+1)]
     return square_list
 n = int(input())
 result = create_square_list(n)
 print(result)
+
